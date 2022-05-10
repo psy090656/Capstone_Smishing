01dc769e24b3c9f27b2e1a1f3352af2f4112f2a5
Seo_JH <kramyu@gmail.com>
2022-05-10 21:10:34 +0900

    2022.05.10 Again

--- app/src/main/java/com/example/anti_smishing/MainActivity.java
+++ app/src/main/java/com/example/anti_smishing/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.anti_smishing;
+import java.util.ArrayList;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.os.Build;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static int PERMISSON_REQUEST_CODE = 1000;
+
+    String permission_list[] = {
+            Manifest.permission_group.SMS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        permissionCheck();
+    }
+
+
+    private void permissionCheck() {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+            ArrayList<String> arrayPermission = new ArrayList<String>();
+
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                arrayPermission.add(Manifest.permission.RECEIVE_SMS);
+            }
+
+            permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                arrayPermission.add(Manifest.permission.READ_SMS);
+            }
+
+            if (arrayPermission.size() > 0) {
+                String strArray[] = new String[arrayPermission.size()];
+                strArray = arrayPermission.toArray(strArray);
+                ActivityCompat.requestPermissions(this, strArray, PERMISSON_REQUEST_CODE);
+            } else {
+                //Initialize Code
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSON_REQUEST_CODE: {
+                if (grantResults.length < 1) {
+                    Toast.makeText(this, "Failed get permission", Toast.LENGTH_SHORT).show();
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                    return;
+                }
+
+                for (int i = 0; i < grantResults.length; i++) {
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission is denied : " + permissions[i], Toast.LENGTH_SHORT).show();
+                        finish();
+                        return;
+                    }
+                }
+
+                Toast.makeText(this, "Permission is granted", Toast.LENGTH_SHORT).show();
+                //Initialize Code
+            }
+            break;
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
