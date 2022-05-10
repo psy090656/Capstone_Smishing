7f85d10704ad91ea22ab65c2c3f50034388a12de
Seo_JH <kramyu@gmail.com>
2022-05-10 21:04:01 +0900

    2022.05.10 Again

---
+++ app/src/main/java/com/example/anti_smishing/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.anti_smishing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
